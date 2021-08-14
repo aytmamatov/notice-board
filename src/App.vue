ebfa647e94ebd4633541d7fadf72d71829bf540d
aytmamatov_musa <black.nobody.anb@gmail.com>
2021-08-14 16:34:58 +0600

    Creating a base structure

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+  <v-app>
+    <v-navigation-drawer app temporary></v-navigation-drawer>
+    <v-app-bar color="deep-purple accent-4" app dense dark>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Notice board</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+    </v-app-bar>
+    <v-content>
+      Hello
+      <router-view />
+    </v-content>
+  </v-app>
 </template>
