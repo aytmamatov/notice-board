19b4ee8c8c7228b3365324f9df850e2cd0f5e617
aytmamatov_musa <black.nobody.anb@gmail.com>
2021-08-22 14:03:19 +0600

    Creating a registration page

--- src/components/Auth/Registration.vue
+++ src/components/Auth/Registration.vue
@@ -1,13 +1,85 @@
 <template>
-  <v-container>
-    <v-layout>
-      <v-flex xs12>Registration</v-flex>
-    </v-layout>
-  </v-container>
+  <v-content>
+    <v-container fluid fill-height>
+      <v-layout align-center justify-center>
+        <v-flex xs12 sm8 md6>
+          <v-card class="elevation-12">
+            <v-toolbar color="primary" dark>
+              <v-toolbar-title color="white">Registration form</v-toolbar-title>
+            </v-toolbar>
+            <v-card-text>
+              <v-form lazy-validation v-model="valid" validation ref="loginForm">
+                <v-text-field
+                  v-model="email"
+                  prepend-icon="mdi-account"
+                  name="email"
+                  label="Email"
+                  type="text"
+                  :rules="emailRules"
+                ></v-text-field>
+                <v-text-field
+                  v-model="password"
+                  prepend-icon="mdi-lock"
+                  name="password"
+                  label="Password"
+                  type="password"
+                  :rules="passwordRules"
+                ></v-text-field>
+                <v-text-field
+                  v-model="confirmPassword"
+                  prepend-icon="mdi-lock"
+                  name="confirm-password"
+                  label="Confirm password"
+                  type="password"
+                  :rules="confirmPasswordRules"
+                ></v-text-field>
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn :disabled="!valid" @click="onSubmit" color="primary">Create account</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-content>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      confirmPassword: "",
+      valid: false,
+      emailRules: [
+        (v) => !!v || "E-mail is required",
+        (v) => !v || /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || "E-mail must be valid",
+      ],
+      passwordRules: [
+        (v) => !!v || "Password is required",
+        (v) => (v && v.length >= 6) || "Password must be more than 6 characters",
+      ],
+      confirmPasswordRules: [
+        (v) => !!v || "Password is required",
+        (v) => v === this.password || "Password should match",
+      ],
+    };
+  },
+  methods: {
+    onSubmit() {
+      if (this.$refs.loginForm.validate()) {
+        const user = {
+          email: this.email,
+          password: this.password,
+        };
+        this.$router.push("/");
+      }
+    },
+  },
+};
 </script>
 
 <style></style>
