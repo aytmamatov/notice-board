bc9090ba061b2f966b31c1fd59b0c56c4b42d8fb
aytmamatov_musa <black.nobody.anb@gmail.com>
2021-09-11 20:20:03 +0600

    Creating an advertising page

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -1,7 +1,20 @@
 <template>
   <v-container>
     <v-layout>
-      <v-flex xs12>Ad</v-flex>
+      <v-flex xs12>
+        <v-card>
+          <v-img src="https://picsum.photos/1300" height="300px"></v-img>
+          <v-card-text>
+            <h1 class="text--primary">Lorem</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, nesciunt.</p>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn class="warning" text>Edit</v-btn>
+            <v-btn class="success">Success</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
