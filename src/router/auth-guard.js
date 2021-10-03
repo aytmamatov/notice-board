83edaa9bad18500133aacfd87ba5a4de997312c9
aytmamatov_musa <black.nobody.anb@gmail.com>
2021-10-03 16:32:38 +0600

    Fixed a bug when logging into the app.

--- src/router/auth-guard.js
+++ src/router/auth-guard.js
@@ -1,7 +1,10 @@
 import store from '@/store';
 
 export default function(to, from, next) {
-  if (store.getters.user) next();
+  if (store.getters.user) {
+    next();
+    return;
+  }
 
   next('/login?loginError=true');
 }
